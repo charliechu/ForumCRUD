a02f460a614a36b0fd51aaf90242d4dd3d0550b5
charliechu <charliechu@techbang.com.tw>
2011-06-21 18:05:08 +0800

    Delete Count of DB operation

--- db/migrate/20110621013233_add_posts_count.rb
+++ db/migrate/20110621013233_add_posts_count.rb
@@ -1,12 +1,6 @@
 class AddPostsCount < ActiveRecord::Migration
   def self.up
     add_column :boards, :posts_count, :integer, :default => 0
-    
-    #each board should show its posts count!
-    Board.reset_column_information
-    Board.find_each do |b|
-      Board.reset_counters b.id, :posts
-    end
   end
 
   def self.down
